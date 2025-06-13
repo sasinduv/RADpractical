f7bda18d6790b26a990ad080ec5f31a2057fb6e3
Sasindu Gihan <sasindugihan123456@gmail.com>
2025-06-13 22:50:25 +0530

    EqualityChecker question

---
+++ EqualityChecker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualityChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (firstNumber == secondNumber)
+            {
+                Console.WriteLine("Two numbers are same");
+            }
+            else
+            {
+                Console.WriteLine("Two numbers are different");
+            }
+
+        }
+    }
+}
