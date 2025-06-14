7dc8b1dc394204d5dd12fe4017c2ebb779fbd79e
Sasindu Gihan <sasindugihan123456@gmail.com>
2025-06-14 21:23:13 +0530

    Question four

---
+++ NumberPyramid/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number of Pyramid");
+            int n = int.Parse(Console.ReadLine());
+
+            int i, j, k;
+            for (i = 0; i < n; i++)
+            {
+                // Print leading spaces
+                for (j = n - i; j > 1; j--)
+                {
+                    Console.Write(" ");
+                }
+                // Print increasing numbers
+                for (j = 0; j <= i; j++)
+                {
+                    Console.Write("*" + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
