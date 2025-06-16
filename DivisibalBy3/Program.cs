6a89d812754115e48cf8750eb87224ad0cac6263
Sasindu Gihan <sasindugihan123456@gmail.com>
2025-06-16 21:23:39 +0530

    Add files via upload

---
+++ DivisibalBy3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisibalBy3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*
+            //create a list of numbers 1 to 100.
+            List<int> numbers = Enumerable.Range(1, 100).ToList();
+            var divisibleBy3 = numbers.Where(n => n % 3 == 0).ToList();
+            Console.WriteLine("Numbers divisible by 3: " + divisibleBy3);
+
+            foreach (var number in divisibleBy3)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+            */
+            Array Numbers = new int[100];
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers.SetValue(i + 1, i);
+            }
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                if ((int)Numbers.GetValue(i) % 3 == 0)
+                {
+                    Console.WriteLine(Numbers.GetValue(i));
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 1, k = 1; // Initialize 'value' to 1 to avoid CS0165 error  
+            Console.WriteLine("Enter a number to calculate its factorial:");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                value = i * value;
+            }
+            Console.WriteLine($"The factorial of {num}! is: {value}");
+        }
+    }
+}
